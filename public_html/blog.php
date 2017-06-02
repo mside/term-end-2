bb05f1d3e81be074b0208172030a70ec011a1e73
mside <michaelside0@gmail.com>
2017-06-02 15:20:48 -0700

    end-end

--- public_html/blog.php
+++ public_html/blog.php
@@ -1,28 +1,35 @@
-
 <!DOCTYPE html>
 <!--[if IE 8]> 				 <html class="no-js lt-ie9" lang="en" > <![endif]-->
 <!--[if gt IE 8]><!--> <html class="no-js" lang="en" > <!--<![endif]-->
 <head>
 	<meta charset="utf-8">
-  <meta name="viewport" content="width=device-width">
-  <title>VANARTS STUDENT MOCK PROJECT</title>
+	<meta name="viewport" content="width=device-width">
+	<title>VANARTS STUDENT MOCK PROJECT</title>
 	<meta name="description" content="This is a student exercise website for the Vancouver Institute of Media Arts (www.vanarts.com)">
 	<link rel="icon" type="image/png"  href="favicon.png">
-<!-- CSS -->
+	<!-- CSS -->
 	<link href="css/foundation/foundation.css" rel="stylesheet" type="text/css">
-  <link href="css/foundation/normalize.css" rel="stylesheet" type="text/css">
+	<link href="css/foundation/normalize.css" rel="stylesheet" type="text/css">
 	<script src="https://use.fontawesome.com/347ac4646f.js"></script>
 	<link href="css/main.css" media="screen, projector, print" rel="stylesheet" type="text/css">
-<!-- modernizr -->
-  <script src="js/vendor/custom.modernizr.js"></script>
+	<!-- modernizr -->
+	<script src="js/vendor/custom.modernizr.js"></script>
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+	<script>
+		(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+		(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+		m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+		})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
+		ga('create', 'UA-100415311-1', 'auto');
+		ga('send', 'pageview');
+	</script>
 </head>
 <body>
 <!-- NAV PARTIAL -->
-	<?php include "partials/header.php"; ?>
+<?php include "partials/header.php"; ?>
 <!-- NAV PARTIAL -->
-	<?php
+<?php
 	$host = "localhost";
 	$user = "michaels_user";
 	$pass = "vanarts";
@@ -34,43 +41,43 @@
 		echo "Connection Failed: " . mysqli_connect_error();
 	}
 	else {
-//get all info
-		$query = "SELECT * FROM blog_table";
-		$resultQuery = mysqli_query($connection, $query);
-		$num = mysqli_num_rows($resultQuery);
-//if info is present in table do the following
-		if($num > 0){
-				while ($row = mysqli_fetch_assoc($resultQuery)) {
-					$id = $row["id"];
-					$title = $row["title"];
-					$date = $row["date"];
-					$image = $row["image"];
-					$imagealt = $row["imagealt"];
-					$text = $row["text"];
+	//get all info
+	$query = "SELECT * FROM blog_table";
+	$resultQuery = mysqli_query($connection, $query);
+	$num = mysqli_num_rows($resultQuery);
+	//if info is present in table do the following
+	if($num > 0){
+		while ($row = mysqli_fetch_assoc($resultQuery)) {
+			$id = $row["id"];
+			$title = $row["title"];
+			$date = $row["date"];
+			$image = $row["image"];
+			$imagealt = $row["imagealt"];
+			$text = $row["text"];
 
-					echo '<div class="blog-title-tag">';
-					echo '<h1>' . $title . '</h1>';
-					echo '<h5>' . $date . '</h5>';
-					echo '</div>';
-					echo '<div class="content-full-width blog-container">';
-					echo '<div class="moving-background-container bottom-spacing">';
-					echo '<div class="centered-div-blog">';
-					echo '<img src="img/' . $image . '" alt="' . $imagealt . '">';
-					echo '</div>';
-					echo '</div>';
-					echo '<div class="row">';
-					echo '<div class="large-8 large-offset-2 columns bottom-spacing">';
-					echo '<p>' . $text . '</p>';
-					echo '</div>';
-					echo '</div>';
-					echo '</div>';
+			echo '<div class="blog-title-tag">';
+			echo '<h1>' . $title . '</h1>';
+			echo '<h5>' . $date . '</h5>';
+			echo '</div>';
+			echo '<div class="content-full-width blog-container">';
+			echo '<div class="moving-background-container bottom-spacing">';
+			echo '<div class="centered-div-blog">';
+			echo '<img src="img/' . $image . '" alt="' . $imagealt . '">';
+			echo '</div>';
+			echo '</div>';
+			echo '<div class="row">';
+			echo '<div class="large-8 large-offset-2 columns bottom-spacing">';
+			echo '<p>' . $text . '</p>';
+			echo '</div>';
+			echo '</div>';
+			echo '</div>';
 			}
 		}
 	}
-	?>
+?>
 <?php include "partials/modal.php"; ?>
 <!-- FOOTER PARTIAL -->
-	<?php include "partials/footer.php"; ?>
+<?php include "partials/footer.php"; ?>
 <!-- FOOTER PARTIAL -->
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 <script src="js/main.js"></script>
--- public_html/log_in_process.php
+++ public_html/log_in_process.php
@@ -2,72 +2,70 @@
   session_start();
 
 
-  // function prevent($this, $conn) {
-	// 	stripslashes($this);
-	// 	mysqli_real_escape_string($conn, $this);
-	// 	return $this;
-	// }
+// function prevent($this, $conn) {
+// 	stripslashes($this);
+// 	mysqli_real_escape_string($conn, $this);
+// 	return $this;
+// }
 
-	if(isset($_POST['usernamelogin']) AND isset($_POST['passwordlogin'])){
+  if(isset($_POST['usernamelogin']) AND isset($_POST['passwordlogin'])){
 
-		$host = "localhost";
-		$user = "michaels_user";
-		$pass = "vanarts";
-		$db = "michaels_mauro_db";
+    $host = "localhost";
+    $user = "michaels_user";
+    $pass = "vanarts";
+    $db = "michaels_mauro_db";
 
-		$connection = mysqli_connect($host, $user, $pass, $db);
+    $connection = mysqli_connect($host, $user, $pass, $db);
 
-		if(!$connection) {
+    if(!$connection) {
       echo "Connection Failed: " . mysqli_connect_error();
-		}
-		else {
-			$username =  $_POST['usernamelogin'];
-			$password =  $_POST['passwordlogin'];
+    }
+    else {
+      $username =  $_POST['usernamelogin'];
+      $password =  $_POST['passwordlogin'];
 
-			// prevent($username, $connection);
-			// prevent($password, $connection);
+// prevent($username, $connection);
+// prevent($password, $connection);
 
-			$select = "SELECT * FROM login_table WHERE user='$username'
+      $select = "SELECT * FROM login_table WHERE user='$username'
       AND password='$password'";
-      // REMOVE WHEN HASHED^^^
+// REMOVE WHEN HASHED^^^
 
-			$selectResult = mysqli_query($connection, $select);
-			$num = mysqli_num_rows($selectResult);
+    $selectResult = mysqli_query($connection, $select);
+    $num = mysqli_num_rows($selectResult);
 
-			if($num > 0) {
-				while ($row = mysqli_fetch_assoc($selectResult)) {
+    if($num > 0) {
+      while ($row = mysqli_fetch_assoc($selectResult)) {
 
-          $_SESSION['username'] = $username;
-          header("location: cms-index.php");
-          //echo "<script> location.replace('cmsMaster.php');</script>";
-
-
-
-          // $hashed_db_password = $row["password"];
-          //
-					// // password_verify()
-					// // Verifies that a password matches a hashed password.
-					// // This is possible because the password_hash() function returns
-					// // the algorithm, cost and salt as part of the returned hash.
-					// // Therefore there is no reason to store in the database the salt
-					// // or algorithm information
-					// //$match = password_verify($password, $hashed_db_password);
-          //
-					// if($match == 0) {
-					// 	echo "No match.<br>";
-					// 	echo "<h1>You Are NOT logged in</h1>";
-					// }
-					// else {
-          //   //sessions variable and go to cms
-          //   //header("location: signUpSuccess.php");
-					// 	echo "There is a match";
-					// 	echo "<h1>Log In SUCCESS</h1>";
-					// }
-				}
-			}
-      mysqli_close($connection);
-		}
-	}
+        $_SESSION['username'] = $username;
+        header("location: cms-index.php");
+//echo "<script> location.replace('cmsMaster.php');</script>";
 
 
+
+// $hashed_db_password = $row["password"];
+//
+// // password_verify()
+// // Verifies that a password matches a hashed password.
+// // This is possible because the password_hash() function returns
+// // the algorithm, cost and salt as part of the returned hash.
+// // Therefore there is no reason to store in the database the salt
+// // or algorithm information
+// //$match = password_verify($password, $hashed_db_password);
+//
+// if($match == 0) {
+// 	echo "No match.<br>";
+// 	echo "<h1>You Are NOT logged in</h1>";
+// }
+// else {
+//   //sessions variable and go to cms
+//   //header("location: signUpSuccess.php");
+// 	echo "There is a match";
+// 	echo "<h1>Log In SUCCESS</h1>";
+// }
+    }
+  }
+  mysqli_close($connection);
+}
+}
 ?>
